938e2eff98666bfc4a5cc394f2145076faf2195c
Mathieu <mathieu.ravaut@gmail.com>
2023-05-17 23:48:46 +0800

    compute rs

--- src/common/utils.py
+++ src/common/utils.py
@@ -46,19 +46,18 @@ def display_scores(mode, scores):
         print("Best {} {} is {:.4f} achieved at iter {} / {}".format(mode, k, best_score_k, best_score_k_idx, len(scores_k)))
 
 
-def compute_r1s(sents):
-    scorer = rouge_scorer.RougeScorer(['rouge1'], use_stemmer=False)
-
-    all_r1s = []
+def compute_rs(sents, metric="rouge1"):
+    scorer = rouge_scorer.RougeScorer([metric], use_stemmer=False)
+    all_rs = []
     for i in range(len(sents)):
         pruned_sents = sents[:i] + sents[(i + 1):]
         pruned_text = " ".join(pruned_sents)
         scores = scorer.score(pruned_text, sents[i])
-        r1 = 100 * scores["rouge1"].fmeasure
-        all_r1s.append(r1)
-    all_r1s = np.array(all_r1s)
+        r = 100 * scores[metric].fmeasure
+        all_rs.append(r)
+    all_rs = np.array(all_rs)
 
-    return all_r1s
+    return all_rs
 
 
 def check_scores(dataset):
